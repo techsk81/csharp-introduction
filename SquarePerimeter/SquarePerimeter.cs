9a678fa815b3cd8bde976863a9c533d7ecfed4bd
Shivani Kapila <65486325+Shvk06@users.noreply.github.com>
2020-12-23 16:35:21 -0500

    Add files via upload

---
+++ SquarePerimeter/SquarePerimeter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquarePerimeter
+{
+    class SquarePerimeter
+    {
+        static void Main(string[] args)
+        {
+            int side = 4;
+
+            int perimeter = 4 * (side);
+
+            Console.WriteLine("Calculated perimeter of the sqaure is: " + perimeter);
+        }
+    }
+}
