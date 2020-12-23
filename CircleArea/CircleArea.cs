6120bb293ad453450581777fca0ca8bfc6f75d5f
Shivani Kapila <65486325+Shvk06@users.noreply.github.com>
2020-12-23 16:36:11 -0500

    Add files via upload

---
+++ CircleArea/CircleArea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleArea
+{
+    class CircleArea
+    {
+        static void Main(string[] args)
+        {
+            double radius, area;
+
+            Console.WriteLine("Enter radius: ");
+
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            //area (circle) = 3.14 x radius x radius
+            area = Math.PI * radius * radius;
+
+            Console.WriteLine("Calculated area of the circle is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
