16116526bc19be1ec2dd4c1e6c303031bfdde5b7
Shivani Kapila <65486325+Shvk06@users.noreply.github.com>
2020-12-23 16:32:00 -0500

    Add files via upload

---
+++ CircleCircumference/CircleCircumference .cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CircleCircumference
+{
+    class CircleCircumference
+    {
+        static void Main(string[] args)
+        {
+
+            double radius, perimeter;
+            
+            Console.WriteLine("Enter radius: ");
+
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            //area (circle) = 3.14 * radius * radius
+            perimeter = 2 * Math.PI * radius;
+
+            Console.WriteLine("Calculated perimeter or circumference of the circle is: " + perimeter);
+            Console.ReadLine();
+            
+        }
+    }
+}
