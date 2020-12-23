b995543069648f61c6eee18a2a93031dc11bb635
Shivani Kapila <65486325+Shvk06@users.noreply.github.com>
2020-12-23 16:33:05 -0500

    Add files via upload

---
+++ CubeVolume/CubeVolume.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CubeVolume
+{
+    class CubeVolume
+    {
+
+        static void Main(string[] args)
+        {
+            int length = 10;
+
+            //volume (Cube) = length x length x length
+            int volume = length * length * length;
+
+            Console.WriteLine("Calculated volume of a cube is: " + volume);
+        }  
+
+    }
+}
