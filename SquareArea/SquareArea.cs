afa1af4e38dfa862de821c9fc0a4a07593db7370
Shivani Kapila <65486325+Shvk06@users.noreply.github.com>
2020-12-23 16:34:00 -0500

    Add files via upload

---
+++ SquareArea/SquareArea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquareArea
+{
+    class SquareArea
+    {
+        static void Main(string[] args)
+        {
+            int side = 16;
+
+            int area = side * side;
+
+            Console.WriteLine("Calculated area of a square is: " + area);
+        }
+        
+    }
+}
